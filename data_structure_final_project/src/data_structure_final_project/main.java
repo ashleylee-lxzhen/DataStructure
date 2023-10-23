135f5ede3b7781c63ef1ee34151a854e6d33b52d
ashleylee-lxzhen <lxzhenivyyy@gmail.com>
2023-10-23 09:50:51 +0800

    init

--- data_structure_final_project/src/data_structure_final_project/main.java
+++ data_structure_final_project/src/data_structure_final_project/main.java
@@ -3,8 +3,7 @@ package data_structure_final_project;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("This is our DS Final Project.");
 	}
 
 }
