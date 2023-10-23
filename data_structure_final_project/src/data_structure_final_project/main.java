121d8cfed368cdb981dd319078e9e1002d970801
ashleylee-lxzhen <lxzhenivyyy@gmail.com>
2023-10-23 09:52:32 +0800

    init

--- data_structure_final_project/src/data_structure_final_project/main.java
+++ data_structure_final_project/src/data_structure_final_project/main.java
@@ -3,7 +3,7 @@ package data_structure_final_project;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("This is our DS Final Project.");
+		System.out.println("This is our DS Final Project!");
 	}
 
 }
